5686b6c42a6f8196976515247d1375d6b2d30486
lachlan <lachlang93@gmail.com>
2021-12-09 17:21:02 +1100

    delete copy

---
+++ models/tickers.py
@@ -0,0 +1,22 @@
+from main import db
+from models.users import Users
+
+# Create a linking table for a many-to-many relationship
+portfolios = db.Table(
+    'portfolios',
+    db.Column('id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('ticker_id', db.String(45), db.ForeignKey('tickers.ticker_id'), primary_key=True)
+)
+# Create a linking table for a many-to-many relationship
+
+class Tickers(db.Model):
+    __tablename__ = "tickers"
+    ticker_id = db.Column(db.String(45), primary_key=True)
+    company_name = db.Column(db.String(45), unique=True, nullable=False)
+    happy_time = db.Column(db.Integer)
+    
+    investors = db.relationship(
+        Users,
+        secondary=portfolios,
+        backref=db.backref('followers')
+    )
---
+++ models/users.py
@@ -0,0 +1,35 @@
+from main import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Users(UserMixin, db.Model):
+
+    __tablename__ = "users"
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    username = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+    password = db.Column(
+        db.String(200),
+        nullable=False
+    )
+
+    @property
+    def image_filename(self):
+        return f"user_images/{self.id}.png"
+
+    # No init method needed, because we have schema.
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+        
+# hide_parameters
