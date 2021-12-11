cbd2e21bacd4f217a7066f0fe20e385b909bf458
lachlan <lachlang93@gmail.com>
2021-12-11 11:27:41 +1100

    add registerable_controllers

---
+++ asxApp/controllers/__init__.py
@@ -0,0 +1,7 @@
+from controllers.user_controller import users
+from controllers.home_controller import home
+from controllers.tickers_controller import tickers
+from controllers.image_controller import user_images
+
+# A list of all avaliable controllers - 
+registerable_controllers = [users, home, tickers, user_images]
