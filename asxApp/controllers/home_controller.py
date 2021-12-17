4f96b4eaeaaf087a5942e56f1f989d96ef83e7ed
l-j-g <lachlang93@gmail.com>
2021-12-17 18:03:06 +1100

    fix page title

--- asxApp/controllers/home_controller.py
+++ asxApp/controllers/home_controller.py
@@ -7,6 +7,6 @@ home = Blueprint('home', __name__)
 @home.route('/home')
 def display_homepage():
     data = {
-        "page_title": "ASX Data - Home Page",
+        "page_title": "Home Page",
     }
     return render_template("home.html", page_data=data)
